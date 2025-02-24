6ac4d662a618bb0df9f6badb8a39e119254afa34
Athenkosi Magada <magadaathenkosi@gmail.com>
2025-02-25 00:42:56 +0200

    chore: move API dependencies to a different file(DependencyInjection)

---
+++ src/careerhive.api/DependencyInjection.cs
@@ -0,0 +1,74 @@
+﻿using System.Text.Json.Serialization;
+using careerhive.application.Interfaces.IRepository;
+using careerhive.domain.Entities;
+using careerhive.infrastructure.Data;
+using careerhive.infrastructure.Repository;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+namespace careerhive.api;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddAPI(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    });
+
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "CareerHive API", Version = "v1" });
+
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                Scheme = "Bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Description = "JWT Authorization header using the Bearer scheme. Paste only the <access-token> bellow."
+            });
+
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[] {}
+                }
+            });
+        });
+
+        services.Configure<ApiBehaviorOptions>(options =>
+        {
+            options.InvalidModelStateResponseFactory = context =>
+            {
+                var error = context.ModelState.Values
+                             .SelectMany(v => v.Errors)
+                             .FirstOrDefault();
+
+                var errorMessage = error?.ErrorMessage ?? "Unknown error has occurred.";
+                return new BadRequestObjectResult(new
+                {
+                    Success = false,
+                    StatusCode = StatusCodes.Status400BadRequest,
+                    Error = errorMessage
+                });
+            };
+        });
+
+        return services;
+    }
+}
--- src/careerhive.api/Program.cs
+++ src/careerhive.api/Program.cs
@@ -9,64 +9,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.OpenApi.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Text.Json.Serialization;
+using careerhive.api;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers()
-    .AddJsonOptions(options =>
-    {
-        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
-    });
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "CareerHive API", Version = "v1" });
-
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Name = "Authorization",
-        Type = SecuritySchemeType.Http,
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Description = "JWT Authorization header using the Bearer scheme.  The token is a JWT (JSON Web Token) access token. Format: <access-token>"
-    });
-
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                }
-            },
-            new string[] {}
-        }
-    });
-});
-
-builder.Services.Configure<ApiBehaviorOptions>(options =>
-{
-    options.InvalidModelStateResponseFactory = context =>
-    {
-        var error = context.ModelState.Values
-                     .SelectMany(v => v.Errors)
-                     .FirstOrDefault();
-
-        var errorMessage = error?.ErrorMessage ?? "Unknown error has occurred.";
-        return new BadRequestObjectResult(new
-        {
-            Success = false,
-            StatusCode = StatusCodes.Status400BadRequest,
-            Error = errorMessage
-        });
-    };
-});
-
+builder.Services.AddAPI(builder.Configuration);
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication(builder.Configuration);
 
@@ -75,7 +23,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1"));
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CareerHive API v1"));
 }
 
 app.UseMiddleware<ExceptionHandlingMiddleware>();
